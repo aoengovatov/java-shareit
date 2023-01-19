6834a8cef72202f86ad63e0b74285a7bef9748ae
Александр <a.o.engovatov@yandex.ru>
2023-01-19 22:56:53 +0400

    добавлена сущность user

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
+    private long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    @Email
+    private String email;
 }
