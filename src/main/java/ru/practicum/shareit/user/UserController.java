fd80c5d8d1d1236c583c455102cc159fc4664a7d
Александр <a.o.engovatov@yandex.ru>
2023-01-19 23:00:19 +0400

    обновлен UserController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,43 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.ItemServiceImpl;
+import ru.practicum.shareit.item.dto.ItemCreateDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+    @Autowired
+    UserServiceImpl userService;
+
+    @GetMapping
+    List<User> getAll(){
+        return userService.getAll();
+    }
+
+    @GetMapping("/{userId}")
+    UserDto getById(@PathVariable Long userId){
+        return userService.getById(userId);
+    }
+    @PostMapping
+    UserDto create(@Valid @RequestBody UserDto userDto) {
+        return userService.create(userDto);
+    }
+
+    @PatchMapping ("/{userId}")
+    UserDto update(@PathVariable Long userId,
+                             @RequestBody UserDto userDto) {
+        return userService.update(userDto, userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    void deleteById(@PathVariable Long userId){
+        userService.deleteById(userId);
+    }
 }
