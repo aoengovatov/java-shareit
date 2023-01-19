110f1e30c4f45ed38a51cd916353368a7929b894
Александр <a.o.engovatov@yandex.ru>
2023-01-19 23:01:17 +0400

    добавлена обработка ошибок

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,65 @@
+package ru.practicum.shareit.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<List<String>> handleIncorrectParameterException(IncorrectParameterException e) {
+        log.warn(e.getMessage());
+        List<String> errors = new ArrayList<>();
+        errors.add(e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List<String>> handleUserNotFoundException(UserNotFoundException e) {
+        log.warn(e.getMessage());
+        List<String> errors = new ArrayList<>();
+        errors.add(e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List<String>> handleItemNotFoundException(ItemNotFoundException e) {
+        log.warn(e.getMessage());
+        List<String> errors = new ArrayList<>();
+        errors.add(e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn(e.getMessage());
+        List<String> errors = new ArrayList<>();
+        errors.add(e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException e) {
+        log.warn(e.getMessage());
+        List<String> errors = new ArrayList<>();
+        errors.add(e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List<String>> handleThrowableException(Throwable e) {
+        log.warn(e.getMessage());
+        List<String> errors = new ArrayList<>();
+        errors.add(e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
