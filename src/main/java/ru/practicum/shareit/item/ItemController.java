be71447b3e82cfaa62202f4f71ea6de789778919
Александр <a.o.engovatov@yandex.ru>
2023-01-22 01:27:53 +0400

    рефакторинг ItemController

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,46 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemCreateDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+
+    public static final String SHARER_USER_ID = "X-Sharer-User-Id";
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping
+    List<ItemDto> getAll(@RequestHeader(value = SHARER_USER_ID, defaultValue = "0") long userId){
+        if(userId > 0){
+            return itemService.getAllByUser(userId);
+        }
+        return itemService.getAll();
+    }
+
+    @GetMapping("/{itemId}")
+    ItemDto getAll(@PathVariable Long itemId){
+        return itemService.getById(itemId);
+    }
+
+    @PostMapping
+    ItemCreateDto createItem (@RequestHeader(SHARER_USER_ID) long userId,
+                        @Valid @RequestBody ItemCreateDto itemDto) {
+        return itemService.create(itemDto, userId);
+    }
+
+    @PatchMapping ("/{itemId}")
+    ItemUpdateDto updateItem(@RequestHeader(SHARER_USER_ID) long userId,
+                             @PathVariable Long itemId,
+                             @RequestBody ItemUpdateDto itemDto) {
+
+        return itemService.update(itemDto, itemId, userId);
+    }
 }
