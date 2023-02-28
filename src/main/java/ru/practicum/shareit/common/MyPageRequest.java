1ec0b599cef12f7ad2d59557eae5496425f0d7d1
Александр <a.o.engovatov@yandex.ru>
2023-02-28 22:14:29 +0400

    Code Style

--- src/main/java/ru/practicum/shareit/common/MyPageRequest.java
+++ src/main/java/ru/practicum/shareit/common/MyPageRequest.java
@@ -6,11 +6,14 @@ import org.springframework.data.domain.Sort;
 public class MyPageRequest extends PageRequest {
 
     int from;
+
     public MyPageRequest(int from, int size, Sort sort) {
         super(from / size, size, sort);
         this.from = from;
     }
 
     @Override
-    public long getOffset() { return from; }
+    public long getOffset() {
+        return from;
+    }
 }
