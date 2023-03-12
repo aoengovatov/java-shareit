12c06d22049fef2b3d90268fe3bbca8d361d0145
Александр <a.o.engovatov@yandex.ru>
2023-03-12 17:51:53 +0400

    тесты ItemServiceImplTest

--- src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -5,9 +5,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
+import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingRepository;
+import ru.practicum.shareit.booking.BookingStatus;
+import ru.practicum.shareit.exception.BadParameterException;
+import ru.practicum.shareit.exception.ItemNotFoundException;
+import ru.practicum.shareit.item.dto.CommentCreateDto;
+import ru.practicum.shareit.item.dto.CommentOutDto;
 import ru.practicum.shareit.item.dto.ItemCreateDto;
 import ru.practicum.shareit.item.dto.ItemOutDto;
+import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.ItemRequestRepository;
@@ -15,10 +23,12 @@ import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -69,8 +79,111 @@ class ItemServiceImplTest {
     }
 
     @Test
-    void update() {
+    void updateItem_whenUserIdNotMatched_thenReturnItemNotFoundException() {
+        long itemId = 0L;
+        long userId = 0L;
+        Item expectedItem = new Item(itemId, "Item name", "Item description", true,
+                new User(1L, "User Name", "email"), null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+
+        assertThrows(ItemNotFoundException.class, () -> itemService.update(
+                ItemMapper.toItemOutDto(expectedItem), itemId, userId));
     }
+
+    @Test
+    void updateItem_whenItemNameIsBlank_thenReturnItemWithOldName() {
+        long itemId = 0L;
+        long userId = 0L;
+        User owner = new User(userId, "User Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(owner));
+        ItemOutDto itemUpdate = ItemMapper.toItemOutDto(expectedItem);
+        itemUpdate.setName("");
+
+        ItemOutDto updatedItem = itemService.update(itemUpdate,itemId, userId);
+
+        assertEquals(expectedItem.getName(), updatedItem.getName());
+    }
+
+    @Test
+    void updateItem_whenItemDescriptionIsBlank_thenReturnItemWithOldDescription() {
+        long itemId = 0L;
+        long userId = 0L;
+        User owner = new User(userId, "User Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(owner));
+        ItemOutDto itemUpdate = ItemMapper.toItemOutDto(expectedItem);
+        itemUpdate.setDescription("");
+
+        ItemOutDto updatedItem = itemService.update(itemUpdate,itemId, userId);
+
+        assertEquals(expectedItem.getDescription(), updatedItem.getDescription());
+    }
+
+    @Test
+    void updateItem_whenItemAvailableIsNull_thenReturnItemWithOldAvailable() {
+        long itemId = 0L;
+        long userId = 0L;
+        User owner = new User(userId, "User Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(owner));
+        ItemOutDto itemUpdate = ItemMapper.toItemOutDto(expectedItem);
+        itemUpdate.setAvailable(null);
+
+        ItemOutDto updatedItem = itemService.update(itemUpdate,itemId, userId);
+
+        assertEquals(expectedItem.getAvailable(), updatedItem.getAvailable());
+    }
+
+    @Test
+    void updateItem_whenItemFieldsIsNull_thenReturnItemWithOldFields() {
+        long itemId = 0L;
+        long userId = 0L;
+        User owner = new User(userId, "User Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(owner));
+        ItemOutDto itemUpdate = ItemMapper.toItemOutDto(expectedItem);
+        itemUpdate.setAvailable(null);
+        itemUpdate.setName(null);
+        itemUpdate.setDescription(null);
+
+        ItemOutDto updatedItem = itemService.update(itemUpdate,itemId, userId);
+
+        assertEquals(expectedItem.getName(), updatedItem.getName());
+        assertEquals(expectedItem.getDescription(), updatedItem.getDescription());
+        assertEquals(expectedItem.getAvailable(), updatedItem.getAvailable());
+    }
+
+    @Test
+    void updateItem_whenItemUpdateAllFields_thenReturnItemWithNewFields() {
+        long itemId = 0L;
+        long userId = 0L;
+        User owner = new User(userId, "User Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(owner));
+        ItemOutDto itemUpdate = ItemMapper.toItemOutDto(expectedItem);
+        itemUpdate.setAvailable(false);
+        itemUpdate.setName("New Name");
+        itemUpdate.setDescription("New Description");
+
+        ItemOutDto updatedItem = itemService.update(itemUpdate,itemId, userId);
+
+        assertEquals(updatedItem.getName(), "New Name");
+        assertEquals(updatedItem.getDescription(), "New Description");
+        assertEquals(updatedItem.getAvailable(), false);
+    }
+
+
 
     @Test
     void getAllItems_whenInvoked_thenReturnedCollectionItems() {
@@ -87,9 +200,17 @@ class ItemServiceImplTest {
     void getItemById_whenItemFound_thenReturnedItem() {
         long itemId = 0L;
         long userId = 0L;
+        long bookerId = 1L;
+        User booker = new User(bookerId, "Booker Name", "email");
         Item expectedItem = new Item(itemId, "Item name", "Item description", true,
                 new User(userId, "User Name", "email"), null);
+        Booking bookingPast = new Booking(0L, LocalDateTime.now().minusDays(2),
+                LocalDateTime.now().minusDays(1), expectedItem, booker, BookingStatus.APPROVED);
+        Booking bookingNext = new Booking(0L, LocalDateTime.now().plusDays(1),
+                LocalDateTime.now().plusDays(2), expectedItem, booker, BookingStatus.APPROVED);
         when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(bookingRepository.getAllBookingByItem(List.of(itemId), BookingStatus.APPROVED,
+                Sort.by(Sort.Direction.DESC, "start"))).thenReturn(List.of(bookingPast, bookingNext));
 
         ItemOutDto actualItem = itemService.getById(itemId, userId);
 
@@ -132,6 +253,43 @@ class ItemServiceImplTest {
     }
 
     @Test
-    void addComment() {
+    void addComment_whenInvoked_thenReturnedComment() {
+        long itemId = 0L;
+        long userId = 0L;
+        long bookerId = 1L;
+        User owner = new User(userId, "User Name", "email");
+        User booker = new User(1L, "Booker Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        Booking booking = new Booking(0L, LocalDateTime.now(), LocalDateTime.now().plusDays(1),
+                expectedItem, booker, BookingStatus.WAITING);
+        Comment expectedComment = new Comment("New comment", itemId, "Booker Name", LocalDateTime.now());
+        CommentCreateDto expectedCommentCreateDto = new CommentCreateDto(expectedComment.getText());
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(bookingRepository.getAllByBookerAndItemId(bookerId, itemId)).thenReturn(List.of(booking));
+        when(commentRepository.save(any())).thenReturn(expectedComment);
+
+        CommentOutDto actualComment = itemService.addComment(expectedCommentCreateDto, booker.getId(), itemId);
+
+        assertEquals(expectedComment.getId(), actualComment.getId());
+        assertEquals(expectedComment.getText(), actualComment.getText());
+        assertEquals(booker.getName(), actualComment.getAuthorName());
+        assertEquals(expectedComment.getCreated(), actualComment.getCreated());
+    }
+
+    @Test
+    void addComment_whenBookingsIsEmpty_thenReturnedBadParameterException() {
+        long itemId = 0L;
+        long userId = 0L;
+        long bookerId = 1L;
+        User owner = new User(userId, "User Name", "email");
+        //User booker = new User(1L, "Booker Name", "email");
+        Item expectedItem = new Item(itemId, "Item name", "Item description",
+                true, owner, null);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(expectedItem));
+        when(bookingRepository.getAllByBookerAndItemId(bookerId, itemId)).thenReturn(Collections.emptyList());
+
+        assertThrows(BadParameterException.class, () -> itemService.addComment(
+                any(), bookerId, itemId));
     }
 }
